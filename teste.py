d34826da7f78dfc014d8658cb4923eb78397cb61
rkujomonteiro <100425100+rkujomonteiro@users.noreply.github.com>
2024-05-02 17:02:10 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,19 +1,38 @@
 import pandas as pd
 import streamlit as st
 
-st.header('Jogo 1')
+col1, col2 = st.columns(2)
 
 dic_odd1 = {}
+with col1:
 
-dic_odd1['0-0'] = st.number_input(' Jogo 1 - 0x0:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd1['1-0'] = st.number_input(' Jogo 1 - 1x0:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd1['0-1'] = st.number_input(' Jogo 1 - 0x1:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd1['1-1'] = st.number_input(' Jogo 1 - 1x1:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd1['2-0'] = st.number_input(' Jogo 1 - 2x0:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd1['0-2'] = st.number_input(' Jogo 1 - 0x2:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd1['2-1'] = st.number_input(' Jogo 1 - 2x1:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd1['1-2'] = st.number_input(' Jogo 1 - 1x2:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd1['2-2'] = st.number_input(' Jogo 1 - 2x2:',min_value=1.0,max_value=1000.0,value=1.0)
+    st.header('Jogo 1')
+    
+    dic_odd1['0-0'] = st.number_input(' Jogo 1 - 0x0:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd1['1-0'] = st.number_input(' Jogo 1 - 1x0:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd1['0-1'] = st.number_input(' Jogo 1 - 0x1:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd1['1-1'] = st.number_input(' Jogo 1 - 1x1:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd1['2-0'] = st.number_input(' Jogo 1 - 2x0:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd1['0-2'] = st.number_input(' Jogo 1 - 0x2:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd1['2-1'] = st.number_input(' Jogo 1 - 2x1:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd1['1-2'] = st.number_input(' Jogo 1 - 1x2:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd1['2-2'] = st.number_input(' Jogo 1 - 2x2:',min_value=1.0,max_value=1000.0,value=1.0)
+
+dic_odd2 = {}
+with col2:
+
+    st.header('Jogo 2')
+    
+    dic_odd2['0-0'] = st.number_input(' Jogo 2 - 0x0:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd2['1-0'] = st.number_input(' Jogo 2 - 1x0:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd2['0-1'] = st.number_input(' Jogo 2 - 0x1:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd2['1-1'] = st.number_input(' Jogo 2 - 1x1:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd2['2-0'] = st.number_input(' Jogo 2 - 2x0:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd2['0-2'] = st.number_input(' Jogo 2 - 0x2:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd2['2-1'] = st.number_input(' Jogo 2 - 2x1:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd2['1-2'] = st.number_input(' Jogo 2 - 1x2:',min_value=1.0,max_value=1000.0,value=1.0)
+    dic_odd2['2-2'] = st.number_input(' Jogo 2 - 2x2:',min_value=1.0,max_value=1000.0,value=1.0)  
+
 
 def dic_prob(dic_odd):
     dic_prob = {}
@@ -23,26 +42,9 @@ def dic_prob(dic_odd):
         prob += dic_prob[o]
     return dic_prob, prob
 
-
 dic_prob1, prob1 = dic_prob(dic_odd1)    
-
-
-st.header('Jogo 2')
-
-dic_odd2 = {}
-
-dic_odd2['0-0'] = st.number_input(' Jogo 2 - 0x0:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd2['1-0'] = st.number_input(' Jogo 2 - 1x0:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd2['0-1'] = st.number_input(' Jogo 2 - 0x1:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd2['1-1'] = st.number_input(' Jogo 2 - 1x1:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd2['2-0'] = st.number_input(' Jogo 2 - 2x0:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd2['0-2'] = st.number_input(' Jogo 2 - 0x2:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd2['2-1'] = st.number_input(' Jogo 2 - 2x1:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd2['1-2'] = st.number_input(' Jogo 2 - 1x2:',min_value=1.0,max_value=1000.0,value=1.0)
-dic_odd2['2-2'] = st.number_input(' Jogo 2 - 2x2:',min_value=1.0,max_value=1000.0,value=1.0)
-
-
 dic_prob2, prob2 = dic_prob(dic_odd2)
+
 
 
 def calculo(dic_odd1,dic_odd2,prob1,prob2):
@@ -86,10 +88,10 @@ def calculo(dic_odd1,dic_odd2,prob1,prob2):
     df_bet.index = df_bet.columns.tolist()
     
     retorno = (df_bet.iloc[0,0] * dic_odd1['0-0'] * dic_odd2['0-0'] - total) / total
-    return df_bet, retorno
+    return df_bet, retorno, total
 
-
-df_bet, retorno = calculo(dic_odd1,dic_odd2,prob1,prob2)
-
-st.dataframe(df_bet)
-st.text(retorno)
+if st.button('Calcular Valores'):
+    df_bet, retorno, total = calculo(dic_odd1,dic_odd2,prob1,prob2)
+    st.dataframe(df_bet)
+    st.text(retorno)
+    st.text(total)
