00229f888035daa180e7bade3524f04d6d1a2e3d
rkujomonteiro <100425100+rkujomonteiro@users.noreply.github.com>
2024-05-02 16:43:08 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,15 +5,15 @@ st.header('Jogo 1')
 
 dic_odd1 = {}
 
-dic_odd1['0-0'] = st.number_input(' Jogo 1 - 0x0:',min_value=1.0,step=None)
-dic_odd1['1-0'] = st.number_input(' Jogo 1 - 1x0:',min_value=1.0,step=None)
-dic_odd1['0-1'] = st.number_input(' Jogo 1 - 0x1:',min_value=1.0,step=None)
-dic_odd1['1-1'] = st.number_input(' Jogo 1 - 1x1:',min_value=1.0,step=None)
-dic_odd1['2-0'] = st.number_input(' Jogo 1 - 2x0:',min_value=1.0,step=None)
-dic_odd1['0-2'] = st.number_input(' Jogo 1 - 0x2:',min_value=1.0,step=None)
-dic_odd1['2-1'] = st.number_input(' Jogo 1 - 2x1:',min_value=1.0,step=None)
-dic_odd1['1-2'] = st.number_input(' Jogo 1 - 1x2:',min_value=1.0,step=None)
-dic_odd1['2-2'] = st.number_input(' Jogo 1 - 2x2:',min_value=1.0,step=None)
+dic_odd1['0-0'] = st.number_input(' Jogo 1 - 0x0:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd1['1-0'] = st.number_input(' Jogo 1 - 1x0:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd1['0-1'] = st.number_input(' Jogo 1 - 0x1:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd1['1-1'] = st.number_input(' Jogo 1 - 1x1:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd1['2-0'] = st.number_input(' Jogo 1 - 2x0:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd1['0-2'] = st.number_input(' Jogo 1 - 0x2:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd1['2-1'] = st.number_input(' Jogo 1 - 2x1:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd1['1-2'] = st.number_input(' Jogo 1 - 1x2:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd1['2-2'] = st.number_input(' Jogo 1 - 2x2:',min_value=1.0,max_value=1000.0,value=1.0)
 
 def dic_prob(dic_odd):
     dic_prob = {}
@@ -31,15 +31,15 @@ st.header('Jogo 2')
 
 dic_odd2 = {}
 
-dic_odd2['0-0'] = st.number_input(' Jogo 2 - 0x0:',min_value=1.0,step=None)
-dic_odd2['1-0'] = st.number_input(' Jogo 2 - 1x0:',min_value=1.0,step=None)
-dic_odd2['0-1'] = st.number_input(' Jogo 2 - 0x1:',min_value=1.0,step=None)
-dic_odd2['1-1'] = st.number_input(' Jogo 2 - 1x1:',min_value=1.0,step=None)
-dic_odd2['2-0'] = st.number_input(' Jogo 2 - 2x0:',min_value=1.0,step=None)
-dic_odd2['0-2'] = st.number_input(' Jogo 2 - 0x2:',min_value=1.0,step=None)
-dic_odd2['2-1'] = st.number_input(' Jogo 2 - 2x1:',min_value=1.0,step=None)
-dic_odd2['1-2'] = st.number_input(' Jogo 2 - 1x2:',min_value=1.0,step=None)
-dic_odd2['2-2'] = st.number_input(' Jogo 2 - 2x2:',min_value=1.0,step=None)
+dic_odd2['0-0'] = st.number_input(' Jogo 2 - 0x0:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd2['1-0'] = st.number_input(' Jogo 2 - 1x0:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd2['0-1'] = st.number_input(' Jogo 2 - 0x1:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd2['1-1'] = st.number_input(' Jogo 2 - 1x1:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd2['2-0'] = st.number_input(' Jogo 2 - 2x0:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd2['0-2'] = st.number_input(' Jogo 2 - 0x2:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd2['2-1'] = st.number_input(' Jogo 2 - 2x1:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd2['1-2'] = st.number_input(' Jogo 2 - 1x2:',min_value=1.0,max_value=1000.0,value=1.0)
+dic_odd2['2-2'] = st.number_input(' Jogo 2 - 2x2:',min_value=1.0,max_value=1000.0,value=1.0)
 
 
 dic_prob2, prob2 = dic_prob(dic_odd2)
